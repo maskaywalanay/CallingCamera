634e25dc1e4e295cb1b98eff6b1808af1d92a13a
maskaywalanay <mwalanay@gmail.com>
2019-09-16 22:16:29 +0700

    tambah kedip text

--- MyApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -6,17 +6,43 @@ import android.view.*;
 import android.view.View.*;
 import android.widget.*;
 import android.content.*;
+import android.animation.*;
+import android.graphics.*;
 
 public class MainActivity extends Activity 
 {
 	// Object yang di bentuk sebagai tombol
 	ImageView img;
+	TextView textView;
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		// menampilkan toast saat pertama di luncurkan
+		Toast.makeText(this,"Clicks A Icon To Launch Camera",Toast.LENGTH_SHORT).show();
+		
+		// menemukan text dengan id
+		textView = (TextView)findViewById(R.id.camera_title);
+		textView.setText("^^ Camera Button ^^");
+		
+		// Membuat text berkedip dengan object animator
+		ObjectAnimator texts = ObjectAnimator.ofInt(textView,"textColor",Color.BLACK,Color.TRANSPARENT);
+		// durasi kedip 1 detik
+		texts.setDuration(1000);
+		texts.setEvaluator(new ArgbEvaluator());
+		// Reverse sedikit kedip
+		// Infinite terus menerus berkedip
+		texts.setRepeatCount(ValueAnimator.INFINITE);
+		
+		// looping kedip 
+		texts.setRepeatMode(ValueAnimator.RESTART);
+		
+		// kedip dimulai
+		texts.start();
 		
 		// menemukan object dengan id 
 		img = (ImageView)findViewById(R.id.img);
